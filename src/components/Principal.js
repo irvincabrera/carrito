40efa0140c8804e99e43ce19425d358aa1b816b8
Irvin Oswaldo Cabrera Ibañez <i.c.ibanez@evomatik.com>
2019-10-07 15:28:48 -0500

    commit inicial
    commit inicial

---
+++ src/components/Principal.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Principal extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-primary">
+                <a className="navbar-brand" href="#">Navbar</a>
+            </nav>
+        )
+    }
+}
+
+export default Principal;
