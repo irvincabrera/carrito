4aaabb8f799b764b8faa740bb1783258a611b245
Irvin Oswaldo Cabrera Ibañez <i.c.ibanez@evomatik.com>
2019-10-10 22:20:49 -0500

    Updates

---
+++ precache-manifest.8f220c9fa68cac88bcc76a245c181066.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2465bce89bac9ccd6c1117c3a3bcf7",
+    "url": "/carrito/index.html"
+  },
+  {
+    "revision": "193455feb769f0d1be9b",
+    "url": "/carrito/static/css/main.1b9bb5fe.chunk.css"
+  },
+  {
+    "revision": "d1e7c10ad9291540213f",
+    "url": "/carrito/static/js/2.01b70e8b.chunk.js"
+  },
+  {
+    "revision": "193455feb769f0d1be9b",
+    "url": "/carrito/static/js/main.ec5d17eb.chunk.js"
+  },
+  {
+    "revision": "98ed50aa5e6e551f362d",
+    "url": "/carrito/static/js/runtime-main.a03c62c7.js"
+  }
+]);
